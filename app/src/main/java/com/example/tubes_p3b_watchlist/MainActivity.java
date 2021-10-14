10020725d56b52c1ed5b7c1dff4785b0e09b9f78
Jordan Alexander <jordan2511alexander@gmail.com>
2021-10-14 12:23:49 +0700

    edit2

--- app/src/main/java/com/example/tubes_p3b_watchlist/MainActivity.java
+++ app/src/main/java/com/example/tubes_p3b_watchlist/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tubes_p3b_watchlist.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
