d55214568a973c388747af9bf02cecec33ba0b3b
putra443 <putra443@gmail.com>
2021-10-15 16:40:38 +0700

    nambah fragment main,method change page (belum beres)

--- app/src/main/java/com/example/tubes_p3b_watchlist/MainActivity.java
+++ app/src/main/java/com/example/tubes_p3b_watchlist/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tubes_p3b_watchlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -8,12 +13,49 @@ import com.example.tubes_p3b_watchlist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    private FragmentManager fragmentManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        this.fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+
+
+
+    }
+
+    public void changePage(int page){
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        if(page==1){
+            if(this.binding..isAdded()){
+                ft.show(this.mainFragment);
+            }
+            else{
+                ft.add(R.id.fragment_container,this.mainFragment);
+            }
+            if(this.secondFragment.isAdded()){
+                ft.hide(this.secondFragment);
+            }
+        }
+        else if(page==2){
+            if(this.secondFragment.isAdded()){
+                ft.show(this.secondFragment);
+            }
+            else{
+                ft.add(R.id.fragment_container,this.secondFragment);
+            }
+            if(this.mainFragment.isAdded()){
+                ft.hide(this.mainFragment);
+            }
+        }
+        ft.commit();
 
     }
 }
---
+++ app/src/main/java/com/example/tubes_p3b_watchlist/MainFragment.java
@@ -0,0 +1,4 @@
+package com.example.tubes_p3b_watchlist;
+
+public class MainFragment {
+}
